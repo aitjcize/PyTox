f50765b7fdd73acca7a59312045e4c2073ef152e
AZ Huang <aitjcize@gmail.com>
2013-12-11 02:15:41 +0800

    Bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name="PyTox",
-    version="0.0.6",
+    version="0.0.7",
     description = 'Python binding for Tox the skype replacement',
     author = 'Wei-Ning Huang (AZ)',
     author_email = 'aitjcize@gmail.com',
