9656ff6388c832e59dd565a3c9084996332a893d
AZ Huang <aitjcize@gmail.com>
2014-04-20 20:29:55 +0800

    Minor fixes.

--- examples/phone.py
+++ examples/phone.py
@@ -1,29 +1,29 @@
 #
 # @file   phone.py
 # @author Wei-Ning Huang (AZ) <aitjcize@gmail.com>
-# 
+#
 # Copyright (C) 2013 - 2014 Wei-Ning Huang (AZ) <aitjcize@gmail.com>
 # All Rights reserved.
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-# 
+#
 
 import sys
 
 import cv2
-import numpy as np 
+import numpy as np
 import pyaudio
 
 from time import sleep
@@ -127,7 +127,7 @@ class AV(ToxAV):
             except Exception as e:
                 print(e)
 
-            sleep(0.01)
+            sleep(0.005)
 
     def audio_decode(self):
         print("Starting audio decode thread...")
@@ -143,7 +143,7 @@ class AV(ToxAV):
             except Exception as e:
                 print(e)
 
-            sleep(0.01)
+            sleep(0.005)
 
     def video_encode(self):
         print("Starting video encode thread...")
