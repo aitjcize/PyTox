e60f79a8920add446c43cd8642ab5e22c244b1de
AZ Huang <aitjcize@gmail.com>
2014-08-16 15:26:38 +0800

    Bump version to 0.0.21

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ else:
 
 setup(
     name="PyTox",
-    version="0.0.20",
+    version="0.0.21",
     description = 'Python binding for Tox the skype replacement',
     author = 'Wei-Ning Huang (AZ)',
     author_email = 'aitjcize@gmail.com',
