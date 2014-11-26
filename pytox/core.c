c787ae1dc3f5f3cdb544cd16f01b5e3dcea4efdc
Guillaume Binet <gbin@google.com>
2014-11-26 11:56:55 -0800

    adjusted types

--- pytox/core.c
+++ pytox/core.c
@@ -98,7 +98,7 @@ static void callback_connection_status(Tox *tox, int32_t friendnumber,
 static void callback_group_invite(Tox *tox, int32_t friendnumber, uint8_t type,
     const uint8_t *data, uint16_t length, void *self)
 {
-  PyObject_CallMethod((PyObject*)self, "on_group_invite", "i#s#", friendnumber,
+  PyObject_CallMethod((PyObject*)self, "on_group_invite", "iiy#", friendnumber,
       type, data, length);
 }
 
