7c6ef965d18910c156347442876a16a53ed129af
iphydf <iphydf@users.noreply.github.com>
2016-09-30 16:10:38 +0100

    Enable debugging in toxcore during travis tests.

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -53,7 +53,7 @@ cd toxcore
 git rev-parse HEAD > toxcore.sha
 if ! ([ -f "$CACHE_DIR/toxcore.sha" ] && diff "$CACHE_DIR/toxcore.sha" toxcore.sha); then
   if [ -f CMakeFiles.txt ]; then
-    cmake -B_build -H. -DCMAKE_INSTALL_PREFIX:PATH=$HOME/cache/usr
+    cmake -B_build -H. -DDEBUG=ON -DSTRICT_ABI=ON -DCMAKE_INSTALL_PREFIX:PATH=$CACHEDIR/usr
   else
     mkdir _build
     autoreconf -fi
